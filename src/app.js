89e03b44360a1d98cff50fc6a5d8438a3560f9aa
Eduardo Vieira <eduardofelipevieira@gmail.com>
2017-07-03 16:36:49 -0300

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,44 @@
+let app = {
+    peakFindingLinear: function (set) {
+        if(set.length === 1) {
+            return set[0];
+        }
+
+        for(let i = 0; i < set.length; i++) {
+            if(set[i] > set[i - 1] && set[i] > set[i + 1]) {
+                return set[i];
+            }
+        }
+    },
+    peakFindingLogarithm: function (set, start, end) {
+        let index = parseInt((start + end) / 2);
+
+        if(index - 1 >= 0 && set[index] < set[index - 1]) {
+            return app.peakFindingLogarithm(set, start, index - 1);
+        } else if(index + 1 <= set.length - 1 && set[index] < set[index + 1]) {
+            return app.peakFindingLogarithm(set, index + 1, end);
+        } else {
+            return set[index];
+        }
+    },
+    insertionSort: function (list) {
+        for(let i = 1; i < list.length; i++) {
+            for(let j = i - 1; j >= 0; j--) {
+                if(list[j + 1] < list[j]) {
+                    let aux = list[j + 1];
+                    list[j + 1] = list[j];
+                    list[j] = aux;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return list;
+    },
+    binarySearch: function (key, list) {
+        return -1;
+    }
+}
+
+module.exports = app;
